d3754efd315c42ef7830c13a29b61714f657dba3
お <104167738+shizumico@users.noreply.github.com>
2024-12-14 19:00:02 +0300

    ✨ Func and Interface for Fiber Response
    🚚 (Migrate): Moved from /utils to /migrate

---
+++ fiber/fiber.go
@@ -0,0 +1,28 @@
+package fiber
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+type ResponseWithError interface {
+	SetErrorMessage(message string)
+	GetErrorMessage() string
+}
+
+func FiberResponse(ctx fiber.Ctx, res ResponseWithError) {
+	body, err := json.Marshal(res)
+	if err != nil {
+		res.SetErrorMessage(err.Error())
+	}
+
+	if res.GetErrorMessage() != "" {
+		ctx.Status(http.StatusInternalServerError)
+	}
+
+	ctx.Set("Content-Type", "application/json")
+
+	_ = ctx.Send(body)
+}
--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,4 +1,4 @@
-package utils
+package migrate
 
 import (
 	"database/sql"
